291b2c9c6ea4900a8690813c4911e84de6f68919
niulka <124307214+niulka@users.noreply.github.com>
2023-03-30 18:09:07 -0300

    Add files via upload

---
+++ FlowLayoutFrame.java
@@ -0,0 +1,89 @@
+import java.awt.FlowLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+public class FlowLayoutFrame extends JFrame
+{
+    private JButton leftJButton;
+    private JButton centerJButton;
+    private JButton rightJButton;
+    private FlowLayout layout;
+    private Container container;
+    
+    public FlowLayoutFrame()
+    {
+        super ("FlowLayout Demo");
+        
+        layout = new FlowLayout();
+        container = getContentPane();
+        setLayout(layout);
+        
+        
+        leftJButton = new JButton("Left");
+        add(leftJButton);
+        leftJButton.addActionListener(
+        
+    new ActionListener()
+    {
+        public void actionperformed(ActionEvent event)
+        {
+            layout.setAlignment(FlowLayout.LEFT);
+            
+            layout.layoutContainer(container);
+            
+        }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+    }
+       
+    );
+        
+centerJButton = new JButton("center");
+add(centerJButton);
+centerJButton.addActionListener(
+        new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+                layout.setAlignment(FlowLayout.CENTER);
+                
+                layout.layoutContainer(container);
+            }
+        }
+);
+
+rightJButton = new JButton("Right");
+add(rightJButton);
+
+rightJButton.addActionListener(
+        new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+                layout.setAlignment(FlowLayout.RIGHT);
+                
+                layout.layoutContainer(container);
+            }
+       }
+);
+    }
+
+    private void add(JButton centerJButton) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void setLayout(FlowLayout layout) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private Container getContentPane() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+    
